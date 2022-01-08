60779522510bcd12511da4f8197379e64c6b4b49
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2022-01-07 17:38:00 -0800

    add guess route and logic

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, app, render_template
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -12,5 +13,18 @@ def home():
     return render_template("index.html", num=rand_num, year=current_year)
 
 
+@app.route("/guess/<name>")
+def guess(name):
+    user_name = name.title()
+    gender = requests.get(f"https://api.genderize.io?name={name}").json()["gender"]
+    age = requests.get(f"https://api.agify.io?name={name}").json()["age"]
+    return render_template(
+        "guess.html",
+        user_name=user_name,
+        gender_guess=gender,
+        age_guess=age,
+    )
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
