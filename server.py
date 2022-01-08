0d15cbd5bec970eabff84d9525a0fb538021bb81
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2022-01-07 18:28:28 -0800

    rf guess route / add blog route

--- server.py
+++ server.py
@@ -10,21 +10,26 @@ app = Flask(__name__)
 def home():
     rand_num = random.randint(1, 10)
     current_year = datetime.datetime.now().year
-    return render_template("index.html", num=rand_num, year=current_year)
+    return render_template("sindex.html", num=rand_num, year=current_year)
 
 
 @app.route("/guess/<name>")
 def guess(name):
-    user_name = name.title()
     gender = requests.get(f"https://api.genderize.io?name={name}").json()["gender"]
     age = requests.get(f"https://api.agify.io?name={name}").json()["age"]
     return render_template(
         "guess.html",
-        user_name=user_name,
+        user_name=name,
         gender_guess=gender,
         age_guess=age,
     )
 
 
+@app.route("/blog")
+def get_blog():
+    posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+    return render_template("blog.html", posts=posts)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
